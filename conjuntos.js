58fea8906ff1e68ec8c86cce32f6ac178c513052
franvasquez9 <89528074+franvasquez9@users.noreply.github.com>
2022-07-04 08:12:50 -0500

    ejercicio 7-1 JS

---
+++ conjuntos.js
@@ -0,0 +1,9 @@
+const familia = ["Francisco", "Jose", "Ana"];
+const nuevoSet = new Set(familia);
+console.log(familia);
+
+nuevoSet.add("Francisco");
+console.log(nuevoSet);
+
+nuevoSet.add("Javascript");
+console.log(nuevoSet);
